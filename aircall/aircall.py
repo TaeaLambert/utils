38187795b0718d68220ccf7ba66835d0bd52b611
Taea Lambert <taea.lambert@hypeanddexter.nz>
2022-10-27 09:23:54 +1300

    update aircall and dialpad methods

--- aircall/aircall.py
+++ aircall/aircall.py
@@ -3,14 +3,18 @@ import requests
 
 class Aircall_client:
     __base_url: str = ""
+    url: str = ""
+    response_data: dict = {}
 
     def __init__(self, access_token) -> None:
         self.__base_url = access_token
 
     def get_aircall_user_by_email(self, email: str) -> dict:
         url = f"{self.__base_url}/v1/users"
+        self.url = "/v1/users"
         try:
             response = requests.get(url)
+            self.response_data = response.json()
             for user in response.json()["users"]:
                 if user["email"] == email:
                     return user
@@ -20,8 +24,10 @@ class Aircall_client:
 
     def get_call_by_id(self, call_id: str) -> dict:
         url = f"{self.__base_url}/v1/calls/{call_id}"
+        self.url = f"/v1/calls/{call_id}"
         try:
             response = requests.get(url)
+            self.response_data = response.json()
             return response.json()
         except Exception as e:
             return e
@@ -29,7 +35,11 @@ class Aircall_client:
     def transfer_call(self, call_id, user_id):
         url = f"{self.__base_url}/v1/calls/{call_id}/transfers"
         data = {"user_id": user_id}
+        self.url = f"/v1/calls/{call_id}/transfers"
         try:
-            requests.post(url, data=data)
+            self.response_data = requests.post(url, data=data).json()
         except Exception as e:
             return e
+
+    def __str__(self):
+        return f"Last url call: {self.url}\n\nLast api response: {str(self.response_data)}"
--- dialpad/dialpad.py
+++ dialpad/dialpad.py
@@ -7,31 +7,40 @@ class Dialpad_client:
         "Accept": "application/json",
         "Authorization": "Bearer ",
     }
+    url: str = ""
+    response_data: dict = {}
 
     def __init__(self, access_token) -> None:
         self.__header["Authorization"] = "Bearer " + access_token
 
     def get_dialpad_user_by_email(self, email: str) -> dict:
-        url = f"https://dialpad.com/api/v2/users?email={email}"
+        self.url = f"https://dialpad.com/api/v2/users?email={email}"
         try:
-            response = requests.get(url, headers=self.__header)
+            response = requests.get(self.url, headers=self.__header)
+            self.response_data = response.json()
             return response.json()
         except Exception as e:
             return e
 
     def get_call_by_id(self, call_id: str) -> dict:
-        url = f"https://dialpad.com/api/v2/call/{call_id}"
+        self.url = f"https://dialpad.com/api/v2/call/{call_id}"
         try:
-            response = requests.get(url, headers=self.__header)
+            response = requests.get(self.url, headers=self.__header)
+            self.response_data = response.json()
             return response.json()
         except Exception as e:
             return e
 
     def transfer_call(self, call_id, phone_to_transfer_to):
-        url = f"https://dialpad.com/api/v2/call/{call_id}/transfer"
+        self.url = f"https://dialpad.com/api/v2/call/{call_id}/transfer"
         payload = {"transfer_to_number": phone_to_transfer_to, "custom_data": "Transferred call"}
 
         try:
-            return requests.post(url, json=payload, headers=self.__header).json()
+            response = requests.post(self.url, json=payload, headers=self.__header)
+            self.response_data = response.json()
+            return response.json()
         except Exception as e:
             return e
+
+    def __str__(self):
+        return f"Last url call: {self.url}\n\nLast api response: {str(self.response_data)}"
