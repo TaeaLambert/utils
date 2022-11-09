0036f8dda09313c9aad8544e2586e0dda40f504b
Taea Lambert <taea.lambert@hypeanddexter.nz>
2022-11-09 16:32:34 +1300

    update firestore for now microapp

--- firestore/firestore.py
+++ firestore/firestore.py
@@ -48,3 +48,43 @@ class Firestore_Client:
                 return None
         else:
             return None
+
+    def unique_num_letter_is_value_in_db(self, portal_id: str, object_type: str, property, value: str) -> bool or None:
+        if self.__client.document(portal_id).get().exists == True:
+            objects = self.__client.document(portal_id).get().to_dict()
+            if object_type in objects:
+                if property in objects[object_type]:
+                    if value in objects[object_type][property]:
+                        return True
+                    else:
+                        return False
+                else:
+                    return None
+            else:
+                return None
+        else:
+            return None
+
+    def unique_num_letter_save_value_to_db(self, portal_id: str, object_type: str, property, value: str) -> bool or None:
+        if self.unique_num_letter_is_value_in_db(portal_id, object_type, property, value) != True:
+
+            value_1 = self.__client.document(portal_id).get()
+            value_2 = self.__client.document(portal_id).get().exists
+
+            if self.__client.document(portal_id).get().exists == True:
+                objects = self.__client.document(portal_id).get().to_dict()
+                if object_type in objects:
+                    if property in objects[object_type]:
+                        objects[object_type][property].append(value)
+                        self.__client.document(portal_id).set(objects)
+                    else:
+                        objects[object_type][property] = [value]
+                        self.__client.document(portal_id).set(objects)
+                else:
+                    objects[object_type] = {property: [value]}
+                    self.__client.document(portal_id).set(objects)
+            else:
+                self.__client.document(portal_id).set({object_type: {property: [value]}})
+        else:
+            return False
+        return True
