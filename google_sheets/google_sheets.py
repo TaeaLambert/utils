7ebf7a0f1cb25e8e5f5a3e1b197954eaccdb6e48
Taea Lambert <taea.lambert@hypeanddexter.nz>
2022-11-23 18:34:41 +1300

    move google script to settings.py

--- google_sheets/google_sheets.py
+++ google_sheets/google_sheets.py
@@ -1,35 +1,13 @@
-import os
 import gspread
+import settings
 import pandas as pd
-from pathlib import Path
-
-
-def google_crential_env_to_file():
-    with open(os.getenv("GOOGLE_CONFIG_LOCATION"), "w") as f:
-        f.write(os.getenv("GOOGLE_APPLICATION_CREDENTIALS"))
 
 
 class google_sheets:
     __client: gspread.Client
 
     def __init__(self):
-        my_file = Path(os.getenv("GOOGLE_CONFIG_LOCATION"))
-        if my_file.is_file():
-            self.__client = gspread.service_account(os.getenv("GOOGLE_CONFIG_LOCATION"))
-        else:
-            google_crential_env_to_file()
-            self.__client = gspread.service_account(os.getenv("GOOGLE_CONFIG_LOCATION"))
-
-    # def download_google_sheet(self, spreadsheetId):
-    #     access_token = self.__client.auth.token
-    #     url = (
-    #         "https://www.googleapis.com/drive/v3/files/"
-    #         + spreadsheetId
-    #         + "/export?mimeType=application%2Fvnd.openxmlformats-officedocument.spreadsheetml.sheet"
-    #     )
-    #     res = requests.get(url, headers={"Authorization": "Bearer " + access_token})
-    #     book = openpyxl.load_workbook(filename=BytesIO(res.content), data_only=False)
-    #     return book
+        self.__client = settings.gc
 
     def download_google_sheet(self, sheet_id, worksheet):
         sh = self.__client.open_by_key(sheet_id)
