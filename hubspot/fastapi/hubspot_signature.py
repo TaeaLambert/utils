025d0cd8a9dbf885adc9733af1592687547264a8
Taea Lambert <taea.lambert@hypeanddexter.nz>
2023-01-27 10:25:18 +1300

    update to siginute to make sure its less that 5 minutes old

--- hubspot/fastapi/hubspot_signature.py
+++ hubspot/fastapi/hubspot_signature.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timedelta
 import json
 import os
 import hmac
@@ -10,12 +11,17 @@ async def verify_hubspot_signature(request: Request, x_hubspot_signature_v3: str
     # print("in verify_hubspot_signature")
     # print(x_hubspot_signature_v3)
     signature = await V3_signature(request)
-    if x_hubspot_signature_v3 != signature:
+    if signature == None:
+        raise HTTPException(status_code=401, detail="This Request is from more than 5 minutes ago")
+    elif x_hubspot_signature_v3 != signature:
         raise HTTPException(status_code=401, detail="The request is not from the right portal.")
 
 
 async def V3_signature(request: Request):
     hub_time_signature = request.headers["X-HubSpot-Request-Timestamp"]
+    if datetime.fromtimestamp(int(hub_time_signature[:-3])) < datetime.now() + timedelta(minutes=-5):
+        return None
+
     client_secret = os.getenv("CLIENT_SECRET")
     request_method = request.method
     if request_method == "POST":
