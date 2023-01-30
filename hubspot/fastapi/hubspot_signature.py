325c242b4fc03fe591745f62e3b5cf19311a257e
Taea Lambert <taea.lambert@hypeanddexter.nz>
2023-01-30 17:41:38 +1300

    testing verify_hubspot_signature

--- hubspot/fastapi/hubspot_signature.py
+++ hubspot/fastapi/hubspot_signature.py
@@ -6,14 +6,23 @@ from base64 import b64encode
 from hashlib import sha256
 from fastapi import HTTPException, Header, Request, status
 
+# Imports Python standard library logging
+import logging
+
 
 async def verify_hubspot_signature(request: Request, x_hubspot_signature_v3: str = Header()):
     # print("in verify_hubspot_signature")
     # print(x_hubspot_signature_v3)
     signature = await V3_signature(request)
     if signature == None:
+        logging.warning(x_hubspot_signature_v3 != signature)
+        logging.warning(x_hubspot_signature_v3)
+        logging.warning(signature)
         raise HTTPException(status.HTTP_429_TOO_MANY_REQUESTS, detail="This Request is from more than 30 seconds ago")
     elif x_hubspot_signature_v3 != signature:
+        logging.warning(x_hubspot_signature_v3 != signature)
+        logging.warning(x_hubspot_signature_v3)
+        logging.warning(signature)
         raise HTTPException(status.HTTP_401_UNAUTHORIZED, detail="The request is not from the right portal.")
 
 
@@ -28,9 +37,11 @@ async def V3_signature(request: Request):
         uri = str(request.url)
         body = await request.body()
         total_string = request_method + uri + body.decode() + hub_time_signature
+        logging.info(total_string)
     else:
         uri = str(request.url)
         total_string = request_method + uri + hub_time_signature
+        logging.info(total_string)
 
     signature = b64encode(hmac.new(key=bytes(client_secret, "utf-8"), msg=bytes(total_string, "utf-8"), digestmod=sha256).digest()).decode()
     return signature
