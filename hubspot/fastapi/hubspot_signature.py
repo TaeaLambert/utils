eac544910271b96edfddc21c55bac1e88505a605
Taea Lambert <taea.lambert@hypeanddexter.nz>
2023-01-30 18:12:22 +1300

    update that i think will get google hosting working

--- hubspot/fastapi/hubspot_signature.py
+++ hubspot/fastapi/hubspot_signature.py
@@ -15,14 +15,8 @@ async def verify_hubspot_signature(request: Request, x_hubspot_signature_v3: str
     # print(x_hubspot_signature_v3)
     signature = await V3_signature(request)
     if signature == None:
-        logging.warning(x_hubspot_signature_v3 != signature)
-        logging.warning(x_hubspot_signature_v3)
-        logging.warning(signature)
         raise HTTPException(status.HTTP_429_TOO_MANY_REQUESTS, detail="This Request is from more than 30 seconds ago")
     elif x_hubspot_signature_v3 != signature:
-        logging.warning(x_hubspot_signature_v3 != signature)
-        logging.warning(x_hubspot_signature_v3)
-        logging.warning(signature)
         raise HTTPException(status.HTTP_401_UNAUTHORIZED, detail="The request is not from the right portal.")
 
 
@@ -33,15 +27,15 @@ async def V3_signature(request: Request):
 
     client_secret = os.getenv("CLIENT_SECRET")
     request_method = request.method
+    uri = str(request.url)
+    if "http://" in uri:
+        uri.replace("http", "https")
+
     if request_method == "POST":
-        uri = str(request.url)
         body = await request.body()
         total_string = request_method + uri + body.decode() + hub_time_signature
-        logging.info(total_string)
     else:
-        uri = str(request.url)
         total_string = request_method + uri + hub_time_signature
-        logging.info(total_string)
 
     signature = b64encode(hmac.new(key=bytes(client_secret, "utf-8"), msg=bytes(total_string, "utf-8"), digestmod=sha256).digest()).decode()
     return signature
