719e0c38fd118a7e01ac53932f67bbd11d22dc46
Taea Lambert <taea.lambert@hypeanddexter.nz>
2023-01-30 18:29:57 +1300

    update verify_hubspot_signature to work on google hosting hopefully

--- hubspot/fastapi/hubspot_signature.py
+++ hubspot/fastapi/hubspot_signature.py
@@ -1,13 +1,9 @@
-from datetime import datetime, timedelta
-import json
 import os
 import hmac
 from base64 import b64encode
 from hashlib import sha256
+from datetime import datetime, timedelta
 from fastapi import HTTPException, Header, Request, status
-
-# Imports Python standard library logging
-import logging
 
 
 async def verify_hubspot_signature(request: Request, x_hubspot_signature_v3: str = Header()):
@@ -15,14 +11,8 @@ async def verify_hubspot_signature(request: Request, x_hubspot_signature_v3: str
     # print(x_hubspot_signature_v3)
     signature = await V3_signature(request)
     if signature == None:
-        logging.warning(x_hubspot_signature_v3 != signature)
-        logging.warning(x_hubspot_signature_v3)
-        logging.warning(signature)
         raise HTTPException(status.HTTP_429_TOO_MANY_REQUESTS, detail="This Request is from more than 30 seconds ago")
     elif x_hubspot_signature_v3 != signature:
-        logging.warning(x_hubspot_signature_v3 != signature)
-        logging.warning(x_hubspot_signature_v3)
-        logging.warning(signature)
         raise HTTPException(status.HTTP_401_UNAUTHORIZED, detail="The request is not from the right portal.")
 
 
@@ -34,17 +24,14 @@ async def V3_signature(request: Request):
     client_secret = os.getenv("CLIENT_SECRET")
     request_method = request.method
     uri = str(request.url)
-    logging.info(uri)
     if "http://" in uri:
-        uri.replace("http", "https")
+        uri = uri.replace("http", "https")
 
     if request_method == "POST":
         body = await request.body()
         total_string = request_method + uri + body.decode() + hub_time_signature
-        logging.info(total_string)
     else:
         total_string = request_method + uri + hub_time_signature
-        logging.info(total_string)
 
     signature = b64encode(hmac.new(key=bytes(client_secret, "utf-8"), msg=bytes(total_string, "utf-8"), digestmod=sha256).digest()).decode()
     return signature
